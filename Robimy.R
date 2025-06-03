480a48e02263d936c6150e56e01ec467951d4064
JagodaOliwia <s202329@student.pg.edu.pl>
2025-06-03 15:33:35 +0200

    bg

--- Robimy.R
+++ Robimy.R
@@ -13,7 +13,28 @@ if (!require("ggplot2")) {
   library(ggplot2)
 }
 
+if (!require("lmtest")) {
+  install.packages("lmtest")
+  library(lmtest)
+}
+
+
 
 #data load
 data <- read_excel("Data_F.xlsx", sheet = "Arkusz1")
+
+#data vis
+
+plot(data$Unemployment , data$HICP_mm)
+plot(data$Pensions , data$HICP_mm)
+plot(data$Healthcare , data$HICP_mm)
+plot(data$Budget_Balance , data$HICP_mm)
+plot(data$New_Housing , data$HICP_mm)
+plot(data$Industry_Orders_mm , data$HICP_mm)
+plot(data$Current_Consumer_Confidence_Indicator , data$HICP_mm)
+plot(data$Trade_Balance , data$HICP_mm)
+
+#statistical tests
+
+
 
