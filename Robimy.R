6dc47bd1ca07b41f1c9955e89140cf99c6a7ac92
JagodaOliwia <s202329@student.pg.edu.pl>
2025-06-03 15:12:28 +0200

    gg

--- Robimy.R
+++ Robimy.R
@@ -15,18 +15,5 @@ if (!require("ggplot2")) {
 
 
 #data load
-data <- read_excel("DATA.xlsx", sheet = "11BEZUK", col_names = FALSE)
-
-# Transpozycja: zamieniamy wiersze z kolumnami
-data_t <- as.data.frame(t(data))
-
-# Pierwszy wiersz staje się nagłówkami
-colnames(data_t) <- data_t[1, ]
-data_t <- data_t[-1, ]
-
-# Teraz mamy ramkę danych: w kolumnach kraje, w wierszach kolejne miesiące
-
-# Dodaj kolumnę daty (np. z pierwszego wiersza)
-data_t$TIME <- seq(as.Date("1996-01-01"), by = "month", length.out = nrow(data_t))
-
+data <- read_excel("Data_F.xlsx", sheet = "Arkusz1")
 
